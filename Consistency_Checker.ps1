b3af43acbfcfcbc5ce343aa8dfedeff33dadaeff
kisoid <vlad.kolesnikov@hotmail.com>
2022-01-10 23:30:38 +0300

    + Consistency_Checker.ps1

---
+++ Consistency_Checker.ps1
@@ -0,0 +1,67 @@
+cls
+
+$workdir = 'T:\!!!_PowerShell_scripts\MyWiki\IT'
+
+[reflection.assembly]::loadwithpartialname("System.Windows.Forms") | Out-Null
+
+$nodes = Get-ChildItem -LiteralPath $workdir -Recurse '*.dcmp2'
+#$nodes
+
+$curr_id = 1
+$node2id = @{}
+
+foreach($node in $nodes)
+{
+    $node2id[$node.BaseName] = $curr_id
+    $curr_id++
+}
+
+$neibs = @{}
+
+$warn_count = 0
+
+foreach($node in $nodes)
+{
+    $richTextBox1 = New-Object System.Windows.Forms.RichTextBox
+    $richTextBox1.LoadFile($node.FullName)
+    $links = ($richTextBox1.Lines | Where-Object {$_ -like '*linkto:*'} | ForEach-Object {$_.Split(':')[1]})
+
+    $tmplist = New-Object System.Collections.Generic.List[System.Object]
+
+    foreach($link in $links)
+    {
+        if(-not $node2id[$link])
+        {
+            $warn_count++
+            Write-Host "ПРЕДУПРЕЖДЕНИЕ: ссылка на несуществующую страницу: $link"
+            continue
+        }
+
+        $tmplist.Add($node2id[$link])
+    }
+
+    $neibs[$node2id[$node.BaseName]] = $tmplist
+}
+
+#$neibs.GetEnumerator() | Out-GridView
+$neibs.Count
+
+$kosyak_count = 0
+
+foreach($Aid in $neibs.Keys)
+{
+    #Write-Host $Aid
+    foreach($Bid in $neibs[$Aid])
+    {
+        if(-not ($neibs[$Bid] -contains $Aid))
+        {
+            $Aname = ($node2id.GetEnumerator() | Where-Object {$_.value -eq $Aid}).Name
+            $Bname = ($node2id.GetEnumerator() | Where-Object {$_.value -eq $Bid}).Name
+            Write-Host "ОШИБКА: отсутствует ссылка: $Bname -> $Aname"
+            $kosyak_count++
+        }
+    }
+}
+
+Write-Host "Ошибки: $kosyak_count"
+Write-Host "Предупреждения: $warn_count"
